08bf9403db849bde6d3ec2e406d6a2852e100e61
sosnowskimilosz <sosnowskimilosz@gmail.com>
2021-11-22 20:30:26 +0100

    initial commit

---
+++ src/main/java/milo/legosetlibrary/LegoSetLibrary/LegoSetLibraryApplication.java
@@ -0,0 +1,13 @@
+package milo.legosetlibrary.LegoSetLibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegoSetLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LegoSetLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/milo/legosetlibrary/LegoSetLibrary/LegoSetLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package milo.legosetlibrary.LegoSetLibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LegoSetLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
