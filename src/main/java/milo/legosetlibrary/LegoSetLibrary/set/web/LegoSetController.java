134f56b82f19f3ad860f2153f44c573f212d38bf
sosnowskimilosz <sosnowskimilosz@gmail.com>
2021-11-28 17:44:36 +0100

    add validation in LegoSetController

--- src/main/java/milo/legosetlibrary/LegoSetLibrary/set/web/LegoSetController.java
+++ src/main/java/milo/legosetlibrary/LegoSetLibrary/set/web/LegoSetController.java
@@ -13,6 +13,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+import javax.validation.Valid;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
@@ -51,7 +56,6 @@ public class LegoSetController {
                 .orElse(ResponseEntity.notFound().build());
     }
 
-
     @GetMapping("/{id}")
     public ResponseEntity<LegoSet> getById(@PathVariable Long id) {
         return service.findById(id)
@@ -61,7 +65,7 @@ public class LegoSetController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public LegoSet addLegoSet(@RequestBody RestLegoSetCommand command) {
+    public LegoSet addLegoSet(@Valid @RequestBody RestLegoSetCommand command) {
         return service.addLegoSet(command.toCreateCommand());
     }
 
@@ -93,12 +97,18 @@ public class LegoSetController {
 
     @Data
     private static class RestLegoSetCommand {
+        @NotBlank(message = "LegoSet should have catalog number")
         String catalogNumber;
+        @NotBlank(message = "LegoSet should have name")
         String title;
+        @NotNull(message = "LegoSet should have status")
         LegoSetStatus status;
         LocalDate purchasingDate;
+        @NotNull(message = "LegoSet should have status")
         LegoCategory category;
+        @Min(0)
         Integer numberOfPieces;
+        @DecimalMin("0.00")
         BigDecimal price;
 
         CreateLegoSetCommand toCreateCommand() {
